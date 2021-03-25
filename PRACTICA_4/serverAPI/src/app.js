2e32f03379b59688cbd47dd80d7cf9ddf5a01de5
Chris <crhalfa09@gmail.com>
2021-03-25 00:42:03 -0600

    Configuracion de Servicios API

--- PRACTICA_4/serverAPI/src/app.js
+++ PRACTICA_4/serverAPI/src/app.js
@@ -1,5 +1,8 @@
+console.log("INICIANDO APLICACION SERVER")
+
 // =========================== CONFIGURACION DE SERVIDOR =====================
-const id_server = process.env.ID_API
+const id_server     = process.env.ID_API
+const port_server   = process.env.PORT_API
 
 // =========================== LEVANTAR APLICACION ===========================
 const express = require("express")
@@ -11,12 +14,12 @@ const axios = require('axios').default
 /**
  *  API SERVER
  */
-app.listen(process.env.PORT_API, () => {
-    console.log("Servicio ESB: 3004");
+app.listen(port_server, () => {
+    console.log("Servicio ESB: " + port_server);
 });
 app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
+app.get('/status', function (req, res) {
     res.send('Servidor ' + id_server + " activo !!!");
     res.status(200)
 });
