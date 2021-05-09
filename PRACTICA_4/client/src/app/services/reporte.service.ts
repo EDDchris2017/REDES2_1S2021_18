22808a30bbc019cc7cd8f41fe9f5ac105f32677f
Mario Morales <mmario_93@hotmail.es>
2021-05-09 16:59:04 -0600

    se agrego la carpeta www a la carpeta loadbalancer de Proyecto

--- PRACTICA_4/client/src/app/services/reporte.service.ts
+++ PRACTICA_4/client/src/app/services/reporte.service.ts
@@ -13,22 +13,22 @@ export class ReporteService {
   guardarReporte(carnet, nombre, curso, cuerpo) {
     const data = { carnet, nombre, curso, cuerpo }
     //return [{ok:true, msg:'Reporte creado', carnet: '1111', nombre: 'nombre1', curso: 'redes2', cuerpo: 'hola ke ace', servidor: '201213283'}]
-    return this.http.post(`${this.API_URL}`, data);
+    return this.http.post(`${this.API_URL}data`, data);
   }
 
   //Todo Recibo los datos del usuario
   obtenerReportes() {
-    return this.http.get(`${this.API_URL}reporte/`)
+    return this.http.get(`${this.API_URL}reporte`)
   }
 
   //Todo: Envio la informacion de los datos para crear reporte de venetos
   guardarReporteEventos(carnet, nombre, nombreEvento, idEvento, extencion, base64) {
     const data = { carnet, nombre, nombreEvento, idEvento, extencion, base64 }
-    return this.http.post(`${this.API_URL}evento/`, data);
+    return this.http.post(`${this.API_URL}evento`, data);
   }
 
   //Todo Recibo los datos de la asistencia
   obtenerReportesAsistencia() {
-    return this.http.get(`${this.API_URL}evento/`)
+    return this.http.get(`${this.API_URL}evento`)
   }
 }
